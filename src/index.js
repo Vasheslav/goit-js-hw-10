623a8e9f41a6b373c834d243fdf77174de78dc00
Viasheslav Rashnik <vasheslav1000@gmail.com>
2023-03-05 01:35:56 +0200

    10

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
-refs = {
+const refs = {
   input: document.querySelector('#search-box'),
   countryList: document.querySelector('.country-list'),
   countryInfo: document.querySelector('.country-info'),
